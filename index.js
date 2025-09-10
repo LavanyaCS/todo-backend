d52b6108761920e30584ced2d26fffc9126aee5d
LavanyaCS <lavanyachandranece@gmail.com>
2025-09-10 18:16:52 +0530

    index cors

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const app = express();
 const cors = require("cors");
 //Middleware
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+// Allow requests from your Netlify frontend
+app.use(cors({
+  origin: "https://todo-frontend-mcjo.netlify.app",  // your Netlify domain
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 require("dotenv").config();
 const port = process.env.PORT || 8080;
 const dbConnection = require("./config/dbConnection");
